6925469f0f3534b2635558de77d01194fec09442
Emanuele Basso <bassoemanuele1704@gmail.com>
2020-06-17 22:07:06 +0200

    Added more query and more info in info Prodotto

--- app/core/query/infoProdottoAlimentare.js
+++ app/core/query/infoProdottoAlimentare.js
@@ -40,20 +40,27 @@ module.exports = async function (request, response){
                     var nome = property.split('#')[1].replace(/([A-Z])/g, ' $1').trim().toLowerCase()
                     var valore = res[iriProdotto][property][0]['value']
 
-                    var obj = {nome: valore}
+                    var obj = {}
+                    obj[nome] = valore
 
                     payload['caratteristiche'].push(obj)
                 }
             }
 
-            var societaVigilante
-            var consorzioTutela
+            var societaVigilante = ""
+            var consorzioTutela = ""
+            var aziendeProduttrici = []
+            var eventi = []
 
             for(var objectIri in res){
                 if((ontologyIri + 'controlla') in res[objectIri]){
                     societaVigilante = objectIri 
                 }else if((ontologyIri + 'valorizza') in res[objectIri]){
                     consorzioTutela = objectIri
+                }else if((ontologyIri + 'produce') in res[objectIri]){
+                    aziendeProduttrici.push(objectIri)
+                }else if((ontologyIri + 'promuove') in res[objectIri]){
+                    eventi.push(objectIri)
                 }
             }
 
@@ -73,8 +80,7 @@ module.exports = async function (request, response){
                         "nomeNazione": res[0]['nomeNazione'].value
                     }
                 }
-            })
-            .then(getInfoConsorzioTutela(consorzioTutela).then((res) =>{
+            }).then(getInfoConsorzioTutela(consorzioTutela).then((res) =>{
                 if(res.length != 0){
                     payload['consorzioTutela'] = {
                         "nome": res[0]['nome'].value,
@@ -88,7 +94,53 @@ module.exports = async function (request, response){
                         "nomeNazione": res[0]['nomeNazione'].value
                     }
                 }
-            })).then(() => {
+            })).then(async () => {
+                payload['aziedeProduttrici'] = []
+
+                await Promise.all(aziendeProduttrici.map(async (element) => {
+                    await getInfoAzienda(element).then((res) => {
+                        if(res.length != 0){
+                            var obj = {
+                                "nome": res[0]['nome'].value,
+                                "indirizzo": res[0]['indirizzo'].value,
+                                "cap": res[0]['cap'].value,
+                                "email": res[0]['email'].value,
+                                "numeroTelefono": res[0]['numeroTelefono'].value,
+                                "nomeCitta": res[0]['nomeCitta'].value,
+                                "nomeProvincia": res[0]['nomeProvincia'].value,
+                                "nomeNazione": res[0]['nomeNazione'].value
+                            }
+                            
+                            payload['aziedeProduttrici'].push(obj)
+                        }
+                    })
+                }))
+            }).then(async () => {
+                payload['eventi'] = []
+
+                await Promise.all(eventi.map(async (element) => {
+                    await getInfoEvento(element).then((res) => {
+                        if(res.length != 0){
+                            var obj = {
+                                "titolo": res[0]['titolo'].value,
+                                "indirizzo": res[0]['indirizzo'].value,
+                                "mese": res[0]['mese'].value,
+                                "organizzatore": res[0]['organizzatore'].value,
+                                "sitoWeb": res[0]['sitoWeb'].value,
+                                "nomeCitta": res[0]['nomeCitta'].value,
+                                "nomeProvincia": res[0]['nomeProvincia'].value,
+                                "nomeNazione": res[0]['nomeNazione'].value
+                            }
+                            
+                            payload['eventi'].push(obj)
+                        }
+                    })
+                }))
+            })
+            
+            
+            
+            .then(() => {
                 logger.info(payload)
                 response.render('infoProdottoAlimentare', {payload})
             })
@@ -245,6 +297,160 @@ function getInfoConsorzioTutela(consorzioTutela){
             }`
 
     query += '<' + consorzioTutela + `> prodotti-qualita:nazione ?nazione.
+            GRAPH ?g3{
+                ?nazione l0:name ?nomeNazione.
+                FILTER(LANG(?nomeNazione) = "it")
+            }}`
+
+    return connection.query(query, true)
+        .then((res) => {
+            return res.results.bindings
+        })
+        .catch((err) => {
+            logger.error(err)
+            return []
+        })
+}
+
+function getInfoAzienda(azienda){
+    /*  QUERY:
+
+            PREFIX prodotti-qualita: <http://www.semanticweb.org/progettoWS/prodotti-qualita#>
+            PREFIX l0: <https://w3id.org/italia/onto/l0/>
+
+            SELECT ?nome, ?indirizzo, ?cap, ?email, ?numeroTelefono, ?nomeCitta, ?nomeProvincia, ?nomeNazione
+
+            FROM NAMED <http://localhost:8890/cities>
+            FROM NAMED <http://localhost:8890/provinces>
+            FROM NAMED <http://localhost:8890/italy>
+
+            WHERE{
+                <azienda> prodotti-qualita:haNome ?nome.
+                <azienda> prodotti-qualita:haIndirizzo ?indirizzo.
+                <azienda> prodotti-qualita:haCAP ?cap.
+                <azienda> prodotti-qualita:haEmail ?email.
+                <azienda> prodotti-qualita:haNumeroDiTelefono ?numeroTelefono.
+
+                <azienda> prodotti-qualita:citta ?citta.
+                GRAPH ?g1{
+                    ?citta l0:name ?nomeCitta.
+                }
+
+                <azienda> prodotti-qualita:provincia ?provincia.
+                GRAPH ?g2{
+                    ?provincia l0:name ?nomeProvincia.
+                }
+
+                <azienda> prodotti-qualita:nazione ?nazione.
+                GRAPH ?g3{
+                    ?nazione l0:name ?nomeNazione.
+                    FILTER(LANG(?nomeNazione) = "it")
+                }
+            }
+    */
+
+    var query = `SELECT ?nome, ?indirizzo, ?cap, ?email, ?numeroTelefono, ?nomeCitta, ?nomeProvincia, ?nomeNazione
+
+                FROM NAMED <http://localhost:8890/cities>
+                FROM NAMED <http://localhost:8890/provinces>
+                FROM NAMED <http://localhost:8890/italy>
+
+                WHERE{`
+
+    query += '<' + azienda + '> prodotti-qualita:haNome ?nome.'
+    query += '<' + azienda + '> prodotti-qualita:haIndirizzo ?indirizzo.'
+    query += '<' + azienda + '> prodotti-qualita:haCAP ?cap.'
+    query += '<' + azienda + '> prodotti-qualita:haEmail ?email.'
+    query += '<' + azienda + '> prodotti-qualita:haNumeroDiTelefono ?numeroTelefono.'
+
+    query += '<' + azienda + `> prodotti-qualita:citta ?citta.
+            GRAPH ?g1{
+                ?citta l0:name ?nomeCitta.
+            }`
+
+    query += '<' + azienda + `> prodotti-qualita:provincia ?provincia.
+            GRAPH ?g2{
+                ?provincia l0:name ?nomeProvincia.
+            }`
+
+    query += '<' + azienda + `> prodotti-qualita:nazione ?nazione.
+            GRAPH ?g3{
+                ?nazione l0:name ?nomeNazione.
+                FILTER(LANG(?nomeNazione) = "it")
+            }}`
+
+    return connection.query(query, true)
+        .then((res) => {
+            return res.results.bindings
+        })
+        .catch((err) => {
+            logger.error(err)
+            return []
+        })
+}
+
+function getInfoEvento(evento){
+    /*  QUERY:
+
+            PREFIX prodotti-qualita: <http://www.semanticweb.org/progettoWS/prodotti-qualita#>
+            PREFIX l0: <https://w3id.org/italia/onto/l0/>
+
+            SELECT ?titolo, ?indirizzo, ?organizzatore, ?mese, ?sitoWeb, ?nomeCitta, ?nomeProvincia, ?nomeNazione
+
+            FROM NAMED <http://localhost:8890/cities>
+            FROM NAMED <http://localhost:8890/provinces>
+            FROM NAMED <http://localhost:8890/italy>
+
+            WHERE{
+                <evento> prodotti-qualita:haTitolo ?titolo.
+                <evento> prodotti-qualita:haIndirizzo ?indirizzo.
+                <evento> prodotti-qualita:organizzatore ?organizzatore.
+                <evento> prodotti-qualita:nelMese ?mese.
+                <evento> prodotti-qualita:haSitoWeb ?sitoWeb.
+
+                <evento> prodotti-qualita:citta ?citta.
+                GRAPH ?g1{
+                    ?citta l0:name ?nomeCitta.
+                }
+
+                <evento> prodotti-qualita:provincia ?provincia.
+                GRAPH ?g2{
+                    ?provincia l0:name ?nomeProvincia.
+                }
+
+                <evento> prodotti-qualita:nazione ?nazione.
+                GRAPH ?g3{
+                    ?nazione l0:name ?nomeNazione.
+                    FILTER(LANG(?nomeNazione) = "it")
+                }
+            }
+    */
+
+    var query = `SELECT ?titolo, ?indirizzo, ?organizzatore, ?mese, ?sitoWeb, ?nomeCitta, ?nomeProvincia, ?nomeNazione
+
+                FROM NAMED <http://localhost:8890/cities>
+                FROM NAMED <http://localhost:8890/provinces>
+                FROM NAMED <http://localhost:8890/italy>
+
+                WHERE{`
+
+    query += '<' + evento + '> prodotti-qualita:haTitolo ?titolo.'
+    query += '<' + evento + '> prodotti-qualita:haIndirizzo ?indirizzo.'
+    query += '<' + evento + '> prodotti-qualita:organizzatore ?organizzatore.'
+    query += '<' + evento + '> prodotti-qualita:nelMese ?mese.'
+    query += '<' + evento + '> prodotti-qualita:haSitoWeb ?sitoWeb.'
+
+    query += '<' + evento + `> prodotti-qualita:citta ?citta.
+            GRAPH ?g1{
+                ?citta l0:name ?nomeCitta.
+            }`
+
+    query += '<' + evento + `> prodotti-qualita:provincia ?provincia.
+            GRAPH ?g2{
+                ?provincia l0:name ?nomeProvincia.
+            }`
+
+    query += '<' + evento + `> prodotti-qualita:nazione ?nazione.
             GRAPH ?g3{
                 ?nazione l0:name ?nomeNazione.
                 FILTER(LANG(?nomeNazione) = "it")
